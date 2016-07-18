f6997d63e9d097f95d5d3967915f11fbd6059279
kimichang <kimichangsign@gmail.com>
2016-07-18 21:47:49 +0800

    Initial commit

---
+++ app/src/main/java/com/newgeniuser/beatbox/BeatBox.java
@@ -0,0 +1,56 @@
+package com.newgeniuser.beatbox;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by KimiChang on 2016/7/18.
+ */
+public class BeatBox {
+    private static final String TAG="BeatBox";
+    private static final String SOUNDS_FOLDER="sample_sounds";
+    private static final int MAX_SOUNDS=5;
+    private AssetManager mAssets;
+    private List<Sound> mSounds;
+    private SoundPool mSoundPool;
+    public BeatBox(Context context){
+        mSoundPool=new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC,0);
+        loadSounds();
+    }
+
+    private void loadSounds(){
+        String[] soundNames={};
+        try{
+            soundNames=mAssets.list(SOUNDS_FOLDER);
+            Log.i(TAG,"Found"+soundNames.length+"sounds");
+        }catch(IOException ioe){
+            Log.e(TAG,"Could not list assets",ioe);
+            return;
+        }
+        for(String filename:soundNames){
+            try{
+            String assetPath=SOUNDS_FOLDER+"/"+filename;
+            Sound sound=new Sound(assetPath);
+            load(sound);
+            mSounds.add(sound);
+        }catch(IOException ioe){
+            Log.e(TAG,"Could not load sound "+ filename,ioe);}
+        }
+    }
+    private void load(Sound sound) throws IOException{
+        AssetFileDescriptor afd=mAssets.openFd(sound.getAssetPath());
+        int soundId=mSoundPool.load(afd,1);
+        sound.setSoundId(soundId);
+    }
+    public List<Sound> getSounds(){
+        return mSounds;
+    }
+}
