941b7cfc79630b2abe83a1599e19394d64689826
kimichang <kimichangsign@gmail.com>
2016-07-19 21:30:43 +0800

    lack of design

--- app/src/main/java/com/newgeniuser/beatbox/BeatBox.java
+++ app/src/main/java/com/newgeniuser/beatbox/BeatBox.java
@@ -19,15 +19,16 @@ public class BeatBox {
     private static final String SOUNDS_FOLDER="sample_sounds";
     private static final int MAX_SOUNDS=5;
     private AssetManager mAssets;
-    private List<Sound> mSounds;
+    private List<Sound> mSounds=new ArrayList<>();
     private SoundPool mSoundPool;
     public BeatBox(Context context){
         mSoundPool=new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC,0);
+        mAssets=context.getAssets();
         loadSounds();
     }
 
     private void loadSounds(){
-        String[] soundNames={};
+        String[] soundNames;
         try{
             soundNames=mAssets.list(SOUNDS_FOLDER);
             Log.i(TAG,"Found"+soundNames.length+"sounds");
@@ -52,5 +53,15 @@ public class BeatBox {
     }
     public List<Sound> getSounds(){
         return mSounds;
+    }
+    public void play(Sound sound){
+        Integer soundId=sound.getSoundId();
+        if(soundId==null){
+            return ;
+        }
+        mSoundPool.play(soundId,1.0f,1.0f,1,0,1.0f);
+    }
+    public void release(){
+        mSoundPool.release();
     }
 }
--- app/src/main/java/com/newgeniuser/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/newgeniuser/beatbox/BeatBoxFragment.java
@@ -22,6 +22,7 @@ public class BeatBoxFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setRetainInstance(true);
         mBeatBox=new BeatBox(getActivity());
     }
 
@@ -34,16 +35,22 @@ public class BeatBoxFragment extends Fragment {
         return view;
     }
 
-    private class SoundHolder extends RecyclerView.ViewHolder{
+    private class SoundHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private Button mButton;
         private Sound mSound;
         public SoundHolder(LayoutInflater inflater,ViewGroup container){
             super(inflater.inflate(R.layout.list_item_sound,container,false));
             mButton=(Button)itemView.findViewById(R.id.list_item_sound_button);
+            mButton.setOnClickListener(this);
         }
+
         public void bindSound(Sound sound){
             mSound=sound;
             mButton.setText(mSound.getName());
+        }
+        @Override
+        public void onClick(View v){
+            mBeatBox.play(mSound);
         }
     }
 
@@ -68,5 +75,10 @@ public class BeatBoxFragment extends Fragment {
         public int getItemCount() {
             return mSounds.size();
         }
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        mBeatBox.release();
     }
 }
